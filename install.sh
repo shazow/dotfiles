292cc666539870499378d907e72f1bae21336e1a
Andrey Petrov <andrey.petrov@shazow.net>
2010-04-25 22:14:10 -0700

    Death to tabs

--- install.sh
+++ install.sh
@@ -8,35 +8,35 @@ cd $OLDPATH
 
 no_all=false
 if [ "$1" == "-n" ]; then
-	no_all=true
+    no_all=true
 fi
 
 function include_into() {
-	if test -e $2 && $(grep -q ". $1" $2);  then
-		echo "Already sourced, skipping: $1 -> $2"
-		return 1
-	fi
+    if test -e $2 && $(grep -q ". $1" $2);  then
+        echo "Already sourced, skipping: $1 -> $2"
+        return 1
+    fi
 
-	echo "Adding source: $1 -> $2"
-	echo ". $1" >> $2
+    echo "Adding source: $1 -> $2"
+    echo ". $1" >> $2
 }
 
 function copy() {
-	if test -e $2; then
-		if $no_all; then
-			echo "Skipping: $2"
-			return
-		fi		
-		read -n1 -p "Target '$2' already exists, overwrite? [y/N] " r
-		if [ "$r" != "" ]; then
-			echo ""
-		fi
-		if [ "$r" != "y" ]; then
-			return
-		fi
-	fi;
-	echo "Copying: $1 -> $2"
-	cp -r "$1" "$2"
+    if test -e $2; then
+        if $no_all; then
+            echo "Skipping: $2"
+            return
+        fi        
+        read -n1 -p "Target '$2' already exists, overwrite? [y/N] " r
+        if [ "$r" != "" ]; then
+            echo ""
+        fi
+        if [ "$r" != "y" ]; then
+            return
+        fi
+    fi;
+    echo "Copying: $1 -> $2"
+    cp -r "$1" "$2"
 }
 
 include_into "$path/.bash_profile" ~/.bash_profile
@@ -49,6 +49,6 @@ copy "$path/.vimrc" ~/.vimrc
 copy "$path/.gvimrc" ~/.gvimrc
 
 if [ "$(uname)" == "Darwin" ]; then
-	include_into "~/.bash_profile" ~/.profile
-	include_into "~/.bashrc" ~/.bash_profile
+    include_into "~/.bash_profile" ~/.profile
+    include_into "~/.bashrc" ~/.bash_profile
 fi
