b7dd00f678352ce6e292fa6fc9bf9c5d1c9556ca
Andrey Petrov <andrey.petrov@shazow.net>
2016-05-09 21:10:32 -0700

    Update plug

--- backup/osx/defaults.sh
+++ backup/osx/defaults.sh
@@ -3,3 +3,6 @@
 # Disable dashboard
 defaults write com.apple.dashboard mcx-disabled -boolean YES
 killall Dock
+
+# disable photo agent
+defaults write com.apple.ImageCapture disableHotPlug -bool YES
